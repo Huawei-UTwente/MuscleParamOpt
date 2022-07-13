f59160280d26ff6b9e007a7304dc007915d027d0
Huawei Wang <h.wang-2@utwente.nl>
2022-07-13 21:20:04 +0200

    fix the bug of wrong muscle force - length equation
    there is a 'square' missing in Friedl's muscle model equation.

--- SupplimentryFunctions/DynFunc/gaussianFunctionAct.m
+++ SupplimentryFunctions/DynFunc/gaussianFunctionAct.m
@@ -4,6 +4,6 @@ function fact = gaussianFunctionAct(lce_nor, b1, b2, b3, b4)
 % Groote 2016 paper
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-fact = b1*exp(-0.5.*(lce_nor - b2).^2./(b3 + b4.*lce_nor));
+fact = b1*exp(-0.5.*(lce_nor - b2).^2./((b3 + b4.*lce_nor).^2));
 
 end
